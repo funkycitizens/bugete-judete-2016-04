16e0de945ffc299fa98a677958728411c6963b05
Alex Morega <alex@grep.ro>
2016-04-26 19:18:58 +0300

    even more metrics

--- chart.js
+++ chart.js
@@ -1,7 +1,10 @@
 export default function chart_js(d3, container, judet) {
 
-  let width = 56000
-  let height = 34200
+  let boxHeight = 34200
+  let paddingTop = 2000
+  let paddingBottom = 2000
+  let width = 55880
+  let height = boxHeight - paddingTop - paddingBottom
 
   var cols = [
     'servicii_publice_generale',
@@ -18,7 +21,7 @@ export default function chart_js(d3, container, judet) {
   ]
   var years = [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014]
   var yearWidth = (width)/years.length
-  var yearSlotLeft = yearWidth/20
+  var yearSlotPadding = yearWidth/20
 
   var color = {
     aparare: '#44529F',
@@ -36,7 +39,7 @@ export default function chart_js(d3, container, judet) {
 
 
   var x = d3.scale.ordinal()
-      .rangeRoundBands([yearSlotLeft, yearWidth], .1)
+      .rangeRoundBands([yearSlotPadding, yearWidth - yearSlotPadding], 0)
 
   var y = d3.scale.linear()
       .range([height, 0])
@@ -49,7 +52,7 @@ export default function chart_js(d3, container, judet) {
       .tickFormat((d) => d ? d : '')
 
   var svg = container.append('g')
-      .attr('transform', `translate(${0},${0})`)
+      .attr('transform', `translate(${0},${paddingTop})`)
 
   x.domain(cols)
   y.domain([0, d3.max(years, (y) => d3.max(cols, (c) => +judet.years[y][c]))])
@@ -75,7 +78,7 @@ export default function chart_js(d3, container, judet) {
 
   eachYear
     .append('g')
-      .attr('transform', `translate(450,${height - 200})`)
+      .attr('transform', `translate(300,${height + 200})`)
     .append('g')
       .attr('transform', 'scale(1.2)')
     .append('path')
@@ -84,7 +87,10 @@ export default function chart_js(d3, container, judet) {
       .attr('d', 'm 12985,3600 c 0,83 -68,150 -151,150 m -5036,0 c -83,0 -150,-67 -150,-150 m 2518,109 -2368,41 m 2669,-41 c -83,0 -151,67 -151,151 0,-84 -67,-151 -150,-151 m 2668,41 -2367,-41')
 
   eachYear.append('g')
-      .attr('transform', `translate(400,${height - 200})`)
+      .attr('transform', `translate(${yearWidth/2 + 150},${height + 1200})`)
+      .append('text')
+        .attr('style', 'font: 700px sans-serif; text-anchor: middle')
+        .text((d) => d)
 
   svg.append('g')
       .attr('class', 'y axis')
